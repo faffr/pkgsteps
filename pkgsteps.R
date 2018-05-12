1886926ebb2eb0f9614437c8820976552a7054b8
Daniel Lee <yangsup@umd.edu>
2018-05-12 08:44:09 -0400

    Update pkgsteps.R

--- pkgsteps.R
+++ pkgsteps.R
@@ -1,20 +1,21 @@
 #STEP FOR CREATING A R PACKAGE ON THE FLY
 #SOURCE: https://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/
 
-#1. INSTALL DEVTOOLS AND LOAD IT IN ORDER TO BE ABLE TO DOWNLOAD PACKAGES FROM GITHUB
+# 1. INSTALL DEVTOOLS AND LOAD IT IN ORDER TO BE ABLE TO DOWNLOAD PACKAGES FROM GITHUB
 #install.packages("devtools")
 library(devtools)
 
-#2. INSTALL ROXYGEN FROM GITHUB AND LOAD IT
+# 2. INSTALL ROXYGEN FROM GITHUB AND LOAD IT
 #install.packages("roxygen2")
 library(roxygen2)
 
-#3. CREATE A DIRECTORY FOR THE PACKAGE 
+# 3. CREATE A DIRECTORY FOR THE PACKAGE 
 setwd("somelocation/Desktop")
 create("packagename") 
 
-#4. CREATE AN .R SCRIPT WITH THE FUNCTION THAT YOU DESIRE TO MAKE INTO A PACKAGE
-#5. ADD DOCUMENTATION AT THE TOP OF THE .R SCRIPT AS FOLLOWS:
+# 4. CREATE AN .R SCRIPT WITH THE FUNCTION THAT YOU DESIRE TO MAKE INTO A PACKAGE
+
+# 5. ADD DOCUMENTATION AT THE TOP OF THE .R SCRIPT AS FOLLOWS:
     #'A title for the package
     #'
     #'Some comment about the package and what it does
@@ -26,8 +27,10 @@ create("packagename")
     #'@examples
     #'examples to make it clear to the user on how to use the parameters vis-a-vis the function with a toy example
 
-#.6 CREATE ANOTHER DOCUMENT NECESSARY FOR THE PACKAGE BY USING THE FOLLOWING LINES      
+# 6. PLACE THE .R SCRIPT IN THE FOLDER THAT WAS JUST CREATED INTO THE FOLDER CALLED "R"  
+
+# 7. SETWD TO THE PACKAGE FOLDER AND USE DOCUMENT() TO CREATE DOCUMENTS NECESSARY FOR THE PACKAGE      
 setwd("somelocation/Desktop/GitHub/packagename") #points to the parent location where the function is located. If this is a change in an existing function, this should point to the GIT clone folder
 document() #creates/edits the .Rd function inside the "man" folder. Do this every time there is a change in the function or the package will not update when it is uploaded to GIT.
 
-#.7 UPLOAD TO GIT USING THE GIT APPLICATION TOOL
+# 8. UPLOAD TO GIT USING THE GIT APPLICATION TOOL
